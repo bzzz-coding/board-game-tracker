353bd558f1a3454da6168a5831e391584b59938b
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-11-09 10:13:18 -0800

    Added .env and set up port

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 2000
+
+const app = express()
+
+app.get('/api/games', (req, res) => {
+  res.send('Get games')
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
