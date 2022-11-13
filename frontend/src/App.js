dda1246c12d89c4a022dbded8f98bd99389f9b6a
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-11-12 23:41:11 -0800

    Added Router and Routes in App.js, installed tailwindCSS and daisyUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/dashboard'
+import Register from './pages/register'
+import Login from './pages/login'
 
 function App() {
   return (
-    <div>
-      <h1>Board Game Tracker</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container mx-auto flex justify-center'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
